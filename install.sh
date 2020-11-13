4dd9600e14799412821baec86491b59784190521
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-11-13 10:03:14 -0500

    update install.sh
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ download_mff() {
 
     curl -LJ0 https://github.com/mut-ex/minimal-functional-fox/archive/master.tar.gz | tar -xz -C /tmp/
 
-    if [[ $? -eq 0 ]]; then
+    if true; then
         echoerr " [>>] Copying..."
 
         USERCHROME="/tmp/minimal-functional-fox-master/userChrome.css"
         USERCONTENT="/tmp/minimal-functional-fox-master/userContent.css"
-        cp -r --backup=simple -t $CHROME_DIRECTORY $USERCHROME $USERCONTENT
+        cp -r --backup=simple -t "$CHROME_DIRECTORY $USERCHROME $USERCONTENT"
         rm -f USERCHROME USERCONTENT
-        cp -r /tmp/minimal-functional-fox-master/* $CHROME_DIRECTORY
+        cp -r /tmp/minimal-functional-fox-master/* "$CHROME_DIRECTORY"
 
-        if [[ $? -eq 0 ]]; then
+        if true; then
             rm -rf /tmp/minimal-functional-fox-master
         else
             echoerr " [!!] There was a problem copying the files. Terminating..."
@@ -50,15 +50,15 @@ MOZILLA_USER_DIRECTORY="$(find ~/.mozilla/firefox -maxdepth 1 -type d -regextype
 if [[ -n $MOZILLA_USER_DIRECTORY ]]; then
     # echoerr "mozilla user directory found: $MOZILLA_USER_DIRECTORY"
 
-    CHROME_DIRECTORY="$(find $MOZILLA_USER_DIRECTORY -maxdepth 1 -type d -name 'chrome')"
+    CHROME_DIRECTORY="$(find "$MOZILLA_USER_DIRECTORY" -maxdepth 1 -type d -name 'chrome')"
 
     if [[ -n $CHROME_DIRECTORY ]]; then
         # echoerr "chrome directory found: ""$CHROME_DIRECTORY"
         download_mff
     else
         echoerr " [>>] No chrome directory found! Creating one..."
-        mkdir $MOZILLA_USER_DIRECTORY"/chrome"
-        if [[ $? -eq 0 ]]; then
+        mkdir "$MOZILLA_USER_DIRECTORY/chrome"
+        if true; then
             CHROME_DIRECTORY="$MOZILLA_USER_DIRECTORY/chrome"
             # echoerr "Directory succesfully created"
             download_mff
